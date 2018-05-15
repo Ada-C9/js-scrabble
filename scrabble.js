80d07d0669ce9b55f1dd77ce4886a58867c6cc9c
LetiTran <leticiastran@gmail.com>
2018-05-15 15:52:14 -0700

    Wrote first idea of score method and passed first test.

--- scrabble.js
+++ scrabble.js
@@ -2,7 +2,40 @@
 const Scrabble = {
   score(word) {
 
+
+    let letterScoreOf1 = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
+    let letterScoreOf2 =  ["D", "G"	];
+    let letterScoreOf3 =  ["B", "C", "M", "P"	];
+    let letterScoreOf4 =  ["F", "H", "V", "W", "Y"];
+    let letterScoreOf5 =  ["K"];
+    let letterScoreOf8 =  ["J", "X"];
+    let letterScoreOf10 =  ["Q", "Z"];
+
+    let letters = word.toUpperCase().split("");
+    let score = 0
+
+    letters.forEach(function(letter) {
+      if (letterScoreOf1.includes(letter)){
+        score ++;
+      } else if (letterScoreOf2.includes(letter)){
+        score += 2;
+      }else if  (letterScoreOf3.includes(letter)){
+        score += 3;
+      }else if  (letterScoreOf4.includes(letter)){
+        score += 4;
+      }else if  (letterScoreOf5.includes(letter)){
+        score += 5;
+      }else if  (letterScoreOf8.includes(letter)){
+        score += 8;
+      }else if  (letterScoreOf10.includes(letter)){
+        score += 10;
+      }
+    });
+
+    return score;
   },
+
+
   highestScoreFrom(arrayOfWords) {
 
   },
@@ -14,3 +47,6 @@ Scrabble.Player = class {
 
 
 module.exports = Scrabble;
+
+
+// Scrabble.score('dog');
