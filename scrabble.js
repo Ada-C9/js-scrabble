6bc78459075bbc40c9f3c8e132a314fac8d66d0c
Kiera <kiera.thomasson@gmail.com>
2018-05-15 16:04:46 -0700

    Created data structure for letter scores. Added to score method and one test is passing.

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,46 @@
+const SCORECHART = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+}
 
 const Scrabble = {
+  // score returns the total score value for the given word.
+  // word input is a string (case sensitive)
   score(word) {
+    let wordUp = word.toUpperCase()
+    let wordSplits = wordUp.split('');
+    let totalScore = 0;
+    wordSplits.forEach(function(letter) {
+      totalScore += SCORECHART[letter]
+    });
 
+    return totalScore
   },
+
   highestScoreFrom(arrayOfWords) {
 
   },
@@ -14,3 +52,5 @@ Scrabble.Player = class {
 
 
 module.exports = Scrabble;
+
+Scrabble.score('dog');
