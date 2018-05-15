da30c6a26ae0acb7d7bd0d4f95651b5fd1f6ca60
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-15 15:20:49 -0700

    added nominal method to score(word), such that it can handle case insensitive inputs, and removed skips from the remaining test within that describe block

--- scrabble.js
+++ scrabble.js
@@ -1,7 +1,35 @@
+const letterValues = {
+  one: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'T'],
+  two: ['D', 'G'],
+  three: ['B', 'C', 'M', 'P'],
+  four: ['F', 'H', 'V', 'W', 'Y'],
+  five: ['K'],
+  eight: ['J', 'X'],
+  ten: ['Q', 'Z']
+};
 
 const Scrabble = {
   score(word) {
-
+    let totalScore = 0;
+    let letters = word.toUpperCase().split("");
+    letters.forEach( function(char) {
+      if (letterValues.one.includes(char)) {
+        totalScore += 1;
+      } else if (letterValues.two.includes(char)) {
+        totalScore += 2;
+      } else if (letterValues.three.includes(char)) {
+        totalScore += 3;
+      } else if (letterValues.four.includes(char)) {
+        totalScore += 4;
+      } else if (letterValues.five.includes(char)) {
+        totalScore += 5;
+      } else if (letterValues.eight.includes(char)) {
+        totalScore += 8;
+      } else if (letterValues.ten.includes(char)) {
+        totalScore += 10;
+      }
+    });
+    return totalScore;
   },
   highestScoreFrom(arrayOfWords) {
 
