cca578e550eb0074bbbe33551994106c89bed42c
Steffany Brown <steffannbrown@gmail.com>
2018-05-15 15:57:16 -0700

    added letterScore helper method and logic to score

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,35 @@
 
 const Scrabble = {
+
   score(word) {
 
+    let letterScore = function letterScore(letter) {
+      let score = Object.keys(scoreBook).find(key => scoreBook[key].includes(letter));
+      return parseInt(score);
+    };
+
+    const scoreBook = {
+      1: ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
+      2: ['d', 'g'],
+      3: ['b', 'c', 'm', 'p'],
+      4: ['f', 'h', 'v', 'w', 'y'],
+      5: ['k'],
+      8: ['j', 'x'],
+      10: ['q', 'z']
+    };
+
+    let word_array = word.toLowerCase().split("");
+    let total = 0
+
+    word_array.forEach(function(letter) {
+      total += letterScore(letter);
+    });
+
+    return total
   },
+
+
+
   highestScoreFrom(arrayOfWords) {
 
   },
@@ -14,3 +41,7 @@ Scrabble.Player = class {
 
 
 module.exports = Scrabble;
+
+
+
+console.log(Scrabble.score("okay"))
