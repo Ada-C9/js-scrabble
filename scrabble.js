408da310a78fd7c78c08a7c0d353fd443c881c41
Tor Shimizu <torshimizu@gmail.com>
2018-05-15 14:48:15 -0700

    added possible_letters and letter_scores constants

--- scrabble.js
+++ scrabble.js
@@ -1,3 +1,44 @@
+const POSSIBLE_LETTERS = {
+  A: 9,
+  N: 6,
+  B: 2,
+  O: 8,
+  C: 2,
+  P: 2,
+  D: 4,
+  Q: 1,
+  E: 12,
+  R: 6,
+  F: 2,
+  S: 4,
+  G: 3,
+  T: 6,
+  H: 2,
+  U: 4,
+  I: 9,
+  V: 2,
+  J: 1,
+  W: 2,
+  K: 1,
+  X: 1,
+  L: 4,
+  Y: 2,
+  M: 2,
+  Z: 1
+}
+
+const LETTER_SCORES = {
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']
+}
+
+
+
 const Scrabble = {
   score(word) {
     // TODO: implement score
