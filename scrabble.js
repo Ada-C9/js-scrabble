cab483e9e2da54c7604bd1ad7adca8f691fdada4
Brenda Rios <riosbrenda@gmail.com>
2018-05-15 15:52:40 -0700

    Initialized Scrabble prototype to get the score function. Score function working.

--- scrabble.js
+++ scrabble.js
@@ -1,16 +1,59 @@
 
-const Scrabble = {
-  score(word) {
-
-  },
-  highestScoreFrom(arrayOfWords) {
-
-  },
+const Scrabble = function () {
+  this.letterValues = { "A": 1, "B":3, "C":3, "D": 2, "E": 1, "F":4, "G":2, "H":4, "I":1, "J":8, "K":5, "L":1,
+  "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8,
+  "Y": 4, "Z": 10 };
 };
 
-Scrabble.Player = class {
+Scrabble.prototype = {
+  score: function(word) {
 
+    if (/^[a-zA-Z]+$/.test(word)) {
+
+      word = word.toUpperCase();
+
+      let wordScore = (word.length == 7)? 50 : 0;
+
+      for (let i = 0; i < word.length; i+=1) {
+
+        wordScore += this.letterValues[word.charAt(i)];
+      }
+
+      return wordScore;
+
+    } else {
+      throw "Please enter a valid input";
+    }
+  },
+
+
+  // highestScoreFrom: function (arrayOfWords) {
+  //   if (Array.isArray(arrayOfWords)) {
+  //     let highestScore = 0,
+  //     // iterate through words array
+  //     arrayOfWords.forEach (function(word) {
+  //       wordScore = this.score(word);
+  //       if (wordScore > highestScore) {
+  //         highestScore = wordScore;
+  //         winner = word;
+  //       } else if (wordScore == highestScore) {
+  //         // winner = function to find the winner;
+  //       }
+  //     });
+  //
+  //     return winner;
+  //   }
+  // },
 };
+
+// Scrabble.Player = class {
+//
+// };
 
 
 module.exports = Scrabble;
+
+
+let mygame = new Scrabble();
+
+console.log(mygame.score("ZZzzZZ"));
