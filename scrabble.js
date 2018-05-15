317235eb5efcab4e2bc180675f5d10b57c1df72e
Samantha Berk <sberk10@gmail.com>
2018-05-15 15:54:16 -0700

    score function

--- scrabble.js
+++ scrabble.js
@@ -1,19 +1,46 @@
 const scores = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z']
+  'A': 1,
+  'E': 1,
+  'I': 1,
+  'O': 1,
+  'U': 1,
+  'L': 1,
+  'N': 1,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'D': 2,
+  'G': 2,
+  'B': 3,
+  'C': 3,
+  'M': 3,
+  'P': 3,
+  'F': 4,
+  'H': 4,
+  'V': 4,
+  'W': 4,
+  'Y': 4,
+  'K': 5,
+  'J': 8,
+  'X': 8,
+  'Q': 10,
+  'Z': 10
 }
 const Scrabble = {
   score(word) {
+    // add error reporting here to check that word is valid
+    word = word.toUpperCase();
+    let word_score = [];
 
-  },
-  highestScoreFrom(arrayOfWords) {
+    for (let i = 0; i < word.length; i += 1) {
+      word_score.push(scores[word[i]]);
+    }
 
+    return word_score.reduce(function(acc, val) { return acc + val; });
   },
+  // highestScoreFrom(arrayOfWords) {
+  //
+  // },
 };
 
 Scrabble.Player = class {
@@ -22,3 +49,4 @@ Scrabble.Player = class {
 
 
 module.exports = Scrabble;
+console.log(Scrabble.score("word"));
