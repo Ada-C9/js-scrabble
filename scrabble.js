2a0c16e396192a07e68fcd9dabb2215833337606
Alexandria Brown <axbn2190@gmail.com>
2018-05-15 16:13:14 -0700

    adds total word score method with input validations

--- scrabble.js
+++ scrabble.js
@@ -1,16 +1,62 @@
 
+
 const Scrabble = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+
   score(word) {
+    let total = 0;
+    if (typeof word != 'string' ) {
+      throw "Invalid word played, must be a string."
+    }
+    for (let letter of word) {
+      let capLetter = letter.toUpperCase();
+      if (this[capLetter]) {
+        total += this[capLetter];
+      } else {
+        throw "Invalid word played, includes invalid letter."
+      }
+    }
 
+    console.log(`Total score for "${word}" is ${total} points.`);
   },
-  highestScoreFrom(arrayOfWords) {
 
-  },
+  // highestScoreFrom(arrayOfWords) {
+  //
+  // },
+
 };
 
-Scrabble.Player = class {
+// Scrabble.Player = class {
+//
+// };
 
-};
 
+// module.exports = Scrabble;
 
-module.exports = Scrabble;
+////
+Scrabble.score('hellothere')
