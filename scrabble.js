33c5b8db6ee1b0cd356be5b22ad6edd4d0c4c495
AngelaPoland <angelalpoland@gmail.com>
2018-05-16 15:25:53 -0700

    tests passing for highestscore function

--- scrabble.js
+++ scrabble.js
@@ -1,83 +1,107 @@
 
 const scoreChart = {
-      'A' : 1,
-      'B' : 3,
-      'C' : 3,
-      'D' : 2,
-      'E' : 1,
-      'F' : 4,
-      'G' : 2,
-      'H' : 4,
-      'I' : 1,
-      'J' : 8,
-      'K' : 5,
-      'L' : 1,
-      'M' : 3,
-      'N' : 1,
-      'O' : 1,
-      'P' : 3,
-      'Q' : 10,
-      'R' : 1,
-      'S' : 1,
-      'T' : 1,
-      'U' : 1,
-      'V' : 4,
-      'W' : 4,
-      'X' : 8,
-      'Y' : 4,
-      'Z' : 10
-    };
+  'A' : 1,
+  'B' : 3,
+  'C' : 3,
+  'D' : 2,
+  'E' : 1,
+  'F' : 4,
+  'G' : 2,
+  'H' : 4,
+  'I' : 1,
+  'J' : 8,
+  'K' : 5,
+  'L' : 1,
+  'M' : 3,
+  'N' : 1,
+  'O' : 1,
+  'P' : 3,
+  'Q' : 10,
+  'R' : 1,
+  'S' : 1,
+  'T' : 1,
+  'U' : 1,
+  'V' : 4,
+  'W' : 4,
+  'X' : 8,
+  'Y' : 4,
+  'Z' : 10
+};
 
 
 const Scrabble = {
- score(word) {
-   word = word.toUpperCase();
+  score(word) {
+    word = word.toUpperCase();
 
-  // check to see if word is a string then raise error if not
+    // check to see if word is a string then raise error if not
 
 
-  if (typeof word !== 'string' || word.length > 7 || word.length < 1) {
+    if (typeof word !== 'string' || word.length > 7 || word.length < 1) {
       throw 'Error! You must enter a word that is 1-7 letters long'
     }
 
-  let regex = /^[a-zA-Z]+$/;
-  if (!regex.test(word)) {
-    throw 'Invalid characters';
-  }
+    let regex = /^[a-zA-Z]+$/;
+    if (!regex.test(word)) {
+      throw 'Invalid characters';
+    }
 
-  let wordScore = 0
+    let wordScore = 0
 
-  // adds 50 points to words that are 7 letters
-  if (word.length === 7) {
-     wordScore += 50;
-   }
+    // adds 50 points to words that are 7 letters
+    if (word.length === 7) {
+      wordScore += 50;
+    }
 
-  let letterArray = word.split('');
+    let letterArray = word.split('');
 
 
-  for (let letter of letterArray) {
-    for (let key in scoreChart) {
-      if (letter === key) {
-      wordScore += scoreChart[key]
+    for (let letter of letterArray) {
+      for (let key in scoreChart) {
+        if (letter === key) {
+          wordScore += scoreChart[key]
+        }
       }
     }
-  }
-  return wordScore
-},
+    return wordScore
+  },
 
 
   highestScoreFrom(arrayOfWords) {
     if (!Array.isArray(arrayOfWords) || arrayOfWords.length === 0) {
       throw 'No words have been given';
-    } else if (arrayOfWords.length === 1) {
+    }
+    else if (arrayOfWords.length === 1) {
       return arrayOfWords[0];
     }
 
-  }
-}
-// Scrabble.Player = class {
-//
-// };
+    let max = 0;
+    let highestWord = '';
 
-// do not comment me out.
-module.exports = Scrabble;
+    for (let i = 0; i < arrayOfWords.length; i++) {
+      let score = this.score(arrayOfWords[i]);
+
+      if (score > max) {
+        max = score;
+        highestWord = arrayOfWords[i];
+      }
+      else if (score === max) {
+        if (highestWord.length !== 7 &&
+          (arrayOfWords[i].length === 7 || (arrayOfWords[i].length < highestWord.length))) {
+            highestWord = arrayOfWords[i];
+          }
+        }
+      }
+
+      return highestWord;
+
+    }
+
+
+  }
+
+  // Scrabble.Player = class {
+  //
+  // };
+
+  // do not comment me out.
+  module.exports = Scrabble;
