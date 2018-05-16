f9f865e8c84df64ee1299123bc1f1de4c90bcaec
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-15 21:44:05 -0700

    calculate score and throws errors for invalid input

--- scrabble.js
+++ scrabble.js
@@ -1,16 +1,54 @@
+const LETTERVALUES = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
 
 const Scrabble = {
-  score(word) {
+  score: function(word) {
+    let score = 0;
+    if (word.length > 7) {
+      throw ('Too long!');
+    } else if (word.length === 7) { score = 50;
+    } else if (word.length === 0) {
+      throw ('Too short!');
+    } else if (!word.match(/^[a-zA-Z]+$/)) {
+      throw ('not a Letter');
+    }
 
-  },
-  highestScoreFrom(arrayOfWords) {
+    let wordArray = word.toUpperCase().split('');
 
+    wordArray.forEach(function (value) {
+      score += LETTERVALUES[value];
+    });
+
+    return score;
   },
 };
-
-Scrabble.Player = class {
-
-};
+Scrabble.score("cat");
 
 
 module.exports = Scrabble;
