622211518776bb9e4a0a2f6bce56e4ec14130418
Zheng Cao <chori84@gmail.com>
2018-05-16 16:21:45 -0700

    Refactor word check in play method

--- scrabble.js
+++ scrabble.js
@@ -85,20 +85,11 @@ Scrabble.Player = class {
       throw 'Error: no word was passed!';
     }
 
-    // TODO: refactor this code
-    const scoreChart = {
-      "A" : 1, "E" : 1, "I" : 1, "O" : 1 , "U" : 1, "L" : 1, "N" : 1, "R" : 1, "S" : 1, "T" : 1,
-      "D" : 2, "G" : 2,
-      "B" : 3, "C" : 3 , "M" : 3, "P" : 3,
-      "F" : 4, "H" : 4, "V" : 4, "W" : 4, "Y" : 4,
-      "K" : 5,
-      "J" : 8, "X" : 8,
-      "Q" : 10, "Z" : 10
-    };
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
     let letters = word.toUpperCase().split('');
     for (let letter of letters) {
-      if (!Object.keys(scoreChart).includes(letter)) {
+      if (!chars.includes(letter)) {
         throw 'Error: invalid word!';
       }
     }
