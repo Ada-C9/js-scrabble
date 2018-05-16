4e29f7a80a65d220396b69ec47da63f96379c2b0
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-05-16 16:47:37 -0700

    Did all the player functions and tests, tottaly forgot about commiting

--- scrabble.js
+++ scrabble.js
@@ -3,6 +3,10 @@
 
 const Scrabble = {
   score(word) {
+    if(word === "" || word == null) {
+      throw new Error ("Must enter a word with at least one character")
+    }
+
     let wordArray = word.toUpperCase().split(``);
     let wordTotal = 0
 
@@ -12,9 +16,7 @@ const Scrabble = {
     if(!/^[a-zA-Z]+$/.test(word)) {
       throw new Error ("Invalid character in the word");
     }
-    if(word === "") {
-      throw new Error ("Must enter a word with at least one character")
-    }
+
 
     wordArray.forEach(function(letter){
       if ([`A`, `E`, `I`, `O`, `U`, `L`, `N`, `R`, `S`, `T`].includes(letter)) {
@@ -68,7 +70,59 @@ const Scrabble = {
 
 };
 
-Scrabble.Player = class {
+Scrabble.Player = class  {
+  constructor(name){
+    this.name = name;
+    this.plays = [];
+    if (this.name == null) {
+      throw new Error (`You must enter a valid name`);
+    }
+  }
+
+  totalScore() {
+    let score = 0
+    this.plays.forEach((num)=>{
+      score += Scrabble.score(num);
+    })
+    return score;
+  }
+
+  hasWon(){
+    if(this.totalScore() >= 100) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  play(word) {
+    if(!/^[a-zA-Z]+$/.test(word)) {
+      throw new Error ("Invalid character in the word");
+    }
+    if(word == null) {
+      throw new Error ("Must enter a word with at least one character")
+    }
+
+    if (this.hasWon()) {
+      return false;
+    }
+    else {
+      this.plays.push(word);
+      return true;
+    }
+  }
+
+  highestScoringWord() {
+    if (this.plays.length === 0) {
+      throw new Error (`You have not played any words`);
+    }
+    return Scrabble.highestScoreFrom(this.plays);
+  }
+
+  highestWordScore() {
+    return Scrabble.score(this.highestScoringWord());
+  }
 
 };
 
