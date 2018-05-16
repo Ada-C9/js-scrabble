2af2e4626d9fd5044b8546cf8f03b3b56a18de28
Brenda Rios <riosbrenda@gmail.com>
2018-05-16 14:47:25 -0700

    Refactored functions to make tests pass for function score.

--- scrabble.js
+++ scrabble.js
@@ -1,12 +1,10 @@
 
-const Scrabble = function () {
-  this.letterValues = { "A": 1, "B":3, "C":3, "D": 2, "E": 1, "F":4, "G":2, "H":4, "I":1, "J":8, "K":5, "L":1,
-  "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8,
-  "Y": 4, "Z": 10 };
-};
+const Scrabble =  {
 
-Scrabble.prototype = {
-  score: function(word) {
+  score(word) {
+    const letterValues = { "A": 1, "B":3, "C":3, "D": 2, "E": 1, "F":4, "G":2, "H":4, "I":1, "J":8, "K":5, "L":1,
+    "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8,
+    "Y": 4, "Z": 10 };
 
     if (/^[a-zA-Z]+$/.test(word)) {
 
@@ -16,34 +14,34 @@ Scrabble.prototype = {
 
       for (let i = 0; i < word.length; i+=1) {
 
-        wordScore += this.letterValues[word.charAt(i)];
+        wordScore += letterValues[word.charAt(i)];
       }
 
       return wordScore;
 
     } else {
-      throw "Please enter a valid input";
+      throw "Please enter a valid input. Word must only contain letters from A-Z";
     }
   },
 
 
-  // highestScoreFrom: function (arrayOfWords) {
-  //   if (Array.isArray(arrayOfWords)) {
-  //     let highestScore = 0,
-  //     // iterate through words array
-  //     arrayOfWords.forEach (function(word) {
-  //       wordScore = this.score(word);
-  //       if (wordScore > highestScore) {
-  //         highestScore = wordScore;
-  //         winner = word;
-  //       } else if (wordScore == highestScore) {
-  //         // winner = function to find the winner;
-  //       }
-  //     });
-  //
-  //     return winner;
-  //   }
-  // },
+// highestScoreFrom: function (arrayOfWords) {
+//   if (Array.isArray(arrayOfWords)) {
+//     let highestScore = 0,
+//     // iterate through words array
+//     arrayOfWords.forEach (function(word) {
+//       wordScore = this.score(word);
+//       if (wordScore > highestScore) {
+//         highestScore = wordScore;
+//         winner = word;
+//       } else if (wordScore == highestScore) {
+//         // winner = function to find the winner;
+//       }
+//     });
+//
+//     return winner;
+//   }
+// },
 };
 
 // Scrabble.Player = class {
@@ -54,6 +52,6 @@ Scrabble.prototype = {
 module.exports = Scrabble;
 
 
-let mygame = new Scrabble();
+// let mygame = new Scrabble();
 
-console.log(mygame.score("ZZzzZZ"));
+console.log(Scrabble.score("ZZzzZZ"));
