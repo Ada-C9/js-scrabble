c78ec74314d2cc762353c419a24248d5c30ca71e
AngelaPoland <angelalpoland@gmail.com>
2018-05-15 18:20:52 -0700

    whoa Javascript. youre trippin me out but i got the first two tests to pass and wrote the score function in its most basic form. does not yet do checks for input

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,61 @@
 
-const Scrabble = {
-  score(word) {
+const scoreChart = {
+      'A' : 1,
+      'B' : 3,
+      'C' : 3,
+      'D' : 2,
+      'E' : 1,
+      'F' : 4,
+      'G' : 2,
+      'H' : 4,
+      'I' : 1,
+      'J' : 8,
+      'K' : 5,
+      'L' : 1,
+      'M' : 3,
+      'N' : 1,
+      'O' : 1,
+      'P' : 3,
+      'Q' : 10,
+      'R' : 1,
+      'S' : 1,
+      'T' : 1,
+      'U' : 1,
+      'V' : 4,
+      'W' : 4,
+      'X' : 8,
+      'Y' : 4,
+      'Z' : 10
+    };
 
-  },
+
+const Scrabble = {
+ score(word) {
+   word = word.toUpperCase();
+
+   // check to see if word is a string then raise error if not
+  // if (typeof word !== 'string') {
+  //
+  // }
+
+  let letterArray = word.split('');
+
+  let wordScore = 0
+
+  for (let letter of letterArray) {
+    for (let key in scoreChart) {
+      if (letter === key) {
+      wordScore += scoreChart[key]
+      }
+    }
+  }
+
+  return wordScore
+
+
+},
+
+
   highestScoreFrom(arrayOfWords) {
 
   },
@@ -12,5 +65,5 @@ Scrabble.Player = class {
 
 };
 
-
+// do not comment me out.
 module.exports = Scrabble;
