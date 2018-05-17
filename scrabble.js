0226e54f0aa816ed64977125a04aa0151686e33a
Cara <cararbrennan@gmail.com>
2018-05-17 13:25:51 -0700

    Wave 1 passing

--- scrabble.js
+++ scrabble.js
@@ -1,16 +1,162 @@
 
 const Scrabble = {
+
+
   score(word) {
 
+    let wordScore = 0
+
+    const SCORING_RUBRIK = {
+      'a': 1,
+      'e': 1,
+      'i': 1,
+      'o': 1,
+      'u': 1,
+      'l': 1,
+      'n': 1,
+      'r': 1,
+      's': 1,
+      't': 1,
+      'd': 2,
+      'g': 2,
+      'b': 3,
+      'c': 3,
+      'm': 3,
+      'p': 3,
+      'f': 4,
+      'h': 4,
+      'v': 4,
+      'w': 4,
+      'y': 4,
+      'k': 5,
+      'j': 8,
+      'x': 8,
+      'q': 10,
+      'z': 10
+    }
+
+
+    for (let i = 0; i < word.length; i++) {
+
+      let letter = word[i];
+
+      if (!RegExp(/^[a-z]/i).test(letter)){
+
+        throw `Letters only please. Letter: ${letter} i == ${i} word: ${word}`
+      }
+
+      wordScore += SCORING_RUBRIK[letter.toLowerCase()];
+    }
+
+    if (word.length === 7){
+      wordScore += 50;
+    }else if (word.length > 7){
+      throw "Words can not be longer than 7 letters.";
+    }else if (word.length === 0){
+      throw "Please submit a word for the game.";
+    }
+
+    return wordScore;
   },
+
+
   highestScoreFrom(arrayOfWords) {
+    let highWord = ''
+    if (arrayOfWords.length === 0){
+      throw "Empty Array of words to choose the highest score from.";
+    }else if (arrayOfWords.length === 1){
+      highWord = arrayOfWords[0];
+    }else {
+      let highScore = -1
 
+      for (let i = 0; i < arrayOfWords.length; i++){
+
+        let compareWord = arrayOfWords[i]
+        let score = this.score(compareWord)
+        if (score > highScore){
+          highWord = compareWord
+          highScore = score
+        }else if (score == highScore){
+          highWord =   Scrabble.tiebreaker(compareWord,highWord)
+        }
+      }
+    }
+    return highWord;
   },
+
+  tiebreaker(challenger, incumbent){
+    if (incumbent.length === 7){
+      return incumbent;
+    }else if (challenger.length === 7){
+      return challenger;
+    }
+
+    if (challenger.length > incumbent.length){
+      return incumbent;
+    }else if (challenger.length < incumbent.length){
+      return challenger;
+    }else if (incumbent.length == challenger.length){
+      return incumbent;
+    }
+
+  }
+
+
 };
 
-Scrabble.Player = class {
+
+Scrabble.Player = class{
+
+  constructor(name, plays = []) {
+    this.name = name;
+    this.plays = plays;
+
+
+    if (name == null){
+      throw "Please enter a name";
+    }
+  }
+
+  scrabbleScore(word) {
+    return Scrabble.score(word);
+//LOOSE COUPLIIIIIIIIIIIIING
+  }
+
+
+  play(word){
+    if (this.hasWon()){
+    //check if it is a word
+    return false;
+}else{
+      if (word.length > 7 || word.length === 0){
+        throw "Words can only be 7 letters long.";
+      }
+
+      this.plays.push(word);
+      return this.plays;
+}
+  }
+
+  totalScore (){
+  let totalScore = 0
+  for(let word in this.plays) {
+    totalScore += this.scrabbleScore(word);
+  }
+  return totalScore;
+
+}
+
+  hasWon(){
+    if (this.totalScore() > 100){
+      return true;
+    }else{
+      return false;
+    }
+  }
 
 };
+
+
 
 
 module.exports = Scrabble;
