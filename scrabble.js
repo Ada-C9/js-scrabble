933f48a7a77465ef2bb5196ebeb14a55249a8fd6
LetiTran <leticiastran@gmail.com>
2018-05-17 22:43:21 -0700

    All required functions and tests passing, but needs refactoring.

--- scrabble.js
+++ scrabble.js
@@ -5,6 +5,7 @@ const Scrabble = {
   score(word) {
 
     //  Letter Scores Table:
+    // change structure!
     let letterScoreOf1 = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
     let letterScoreOf2 =  ["D", "G"	];
     let letterScoreOf3 =  ["B", "C", "M", "P"	];
@@ -57,28 +58,90 @@ const Scrabble = {
 
   highestScoreFrom(arrayOfWords) {
 
+    // Vadilations:
     if (arrayOfWords.length == 0){
-      throw `No words given!`;
+      throw `No words have been played yet!`;
     } else if (!Array.isArray(arrayOfWords)){
       throw `This is not an array!`;
     } else if (arrayOfWords.length == 1){
       return arrayOfWords[0];
     }
 
-    // let arrayOfScores = [];
-    // arrayOfWords.forEach(function(word) {
-    //   arrayOfScores.push(this.score(word));
-    // });
-    //
-    // let indexOfMaxValue = arrayOfScores.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
-    //
-    // return arrayOfWords[indexOfMaxValue];
+    // Define word with higest score
+    let maxScoredWord = ['', 0];
+    arrayOfWords.forEach(function (word) {
+      let thisWordScore = Scrabble.score(word);
 
+      if (thisWordScore > maxScoredWord[1]) {
+        maxScoredWord[0] = word;
+        maxScoredWord[1] = thisWordScore;
+      } else if (thisWordScore === maxScoredWord[1]) {
+        if (word.length === 7 || (word.length < maxScoredWord[0].length && maxScoredWord[0].length !== 7)) {
+          maxScoredWord[0] = word;
+          maxScoredWord[1] = thisWordScore;
+        }
+      }
+
+    });
+    // returns the highest scoring word played
+    return maxScoredWord[0];
   },
 };
-
 Scrabble.Player = class {
+  constructor(name) {
+    // Require a name for palyer:
+    if (!name) {
+      throw ('player must have a name');
+    } else {
+      this.name = name;
+    }
+    // Coolection to record the played words:
+    this.plays = [];
+  }
 
+  play(word) {
+    // Require a real word:
+    if (!word || (typeof word !== 'string')) {
+      throw ('Please enter a real word! (Must have letters and cannot have numbers..)');
+      // Returns false and does not update plays if the player has already won:
+    } else if (this.hasWon()) {
+      return false;
+    } else {
+      // save palyed word:
+      this.plays.push(word);
+      return true;
+    }
+  }
+
+  plays() {
+    return this.plays;
+  }
+
+  totalScore() {
+    // Starts at zero and then add up with each stored played word:
+    let totalScore = 0;
+    // use arrow function.....
+    this.plays.forEach(function (word) {
+      totalScore += Scrabble.score(word);
+    });
+
+    return totalScore;
+  }
+
+  hasWon() {
+    // Player wins when scores 100 or more:
+    return this.totalScore() >= 100 ? true : false;
+  }
+
+  highestScoringWord() {
+    // returns the highest scoring word played:
+    return Scrabble.highestScoreFrom(this.plays);
+  }
+
+  highestWordScore() {
+    // returns the score of the highest scoring word played:
+    return Scrabble.score(this.highestScoringWord());
+  }
 };
 
 
